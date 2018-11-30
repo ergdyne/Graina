f3c9c1007c4d3385fcdb735f41cd67277045c1db
j <github@ergdyne.com>
2018-11-30 12:56:36 -0500

    mid($App): grain flow prototyping

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from './components/Grid'
-import { distance } from './functions/cellData'
+import { distance, findCell } from './functions/cellData'
 import {gridProps} from './functions/gridMath'
 import style from './App.css'
 import logo from './logo.svg'
@@ -21,7 +21,7 @@ export default class App extends React.Component {
       user:{
         x:2,
         y:2,
-        clicks:100,
+        clicks:1000,
         color:{r:255,g:0,b:255}
       },
       grainCost: 2,
@@ -33,6 +33,8 @@ export default class App extends React.Component {
 
     this.updateGrid = this.updateGrid.bind(this)
     this.updateUser = this.updateUser.bind(this)
+    this.insertGrain = this.insertGrain.bind(this)
+    this.checkData = this.checkData.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
 
@@ -50,14 +52,61 @@ export default class App extends React.Component {
       user:{
         x: x,
         y: y,
-        clicks: (this.state.user.clicks - changeClicks)
+        clicks: (this.state.user.clicks - changeClicks),
+        color: this.state.user.color
       }
     })
     //ehhh this is all hacked together for testing so!
     this.updateGrid(gridProps(this.state.gridXSize,this.state.gridYSize,x,y))
   }
+
+  checkData(data){
+    //find cells with over 5 grains
+    const heavyCells = data.filter(c=> c.grains.length > 4)
+    //all have at least 5 grains
+    if(heavyCells.length>0){
+      //there is at least one such cell
+      const cell = heavyCells[0]
+      const grains = cell.grains
+      //take any excess grains... this mutates the grain
+      const newGrains = grains.splice(3,cell.grains.length-4)
+      
+      //create new cell with it
+      const newCell = { x:cell.x,y:cell.y,grains:newGrains}
+      //put new cell into the set
+      const newSet = data.filter(c=> !(c.x===cell.x && c.y===cell.y)).concat([newCell])
+      //send each grain to the right location
+      const fullyNewSet =
+        this.insertGrain( 
+          this.insertGrain(
+              this.insertGrain(
+                this.insertGrain(newSet,cell.x+1,cell.y,grains[0]
+                ),cell.x,cell.y+1,grains[1]
+              ),cell.x-1,cell.y,grains[2]
+            ),cell.x,cell.y-1,grains[3]
+        )
+      this.setState({data:fullyNewSet})
+    }
+  }
+
   componentDidUpdate(){
-    console.log(this.state.user)
+    this.checkData(this.state.data)
+  }
+
+  insertGrain(data,x,y,grain){
+    const cell = findCell(x,y,data)
+    const sansCell = data.filter(c=> !(c.x===cell.x && c.y===cell.y))
+    const newGrains = () => {
+      if(cell.grains.length>0){
+        return cell.grains
+      }return []}
+
+    const newCell = 
+      {
+        x:cell.x,y:cell.y,grains:(newGrains().concat([grain]))
+      }
+    return (sansCell.concat([newCell]))
+
   }
 
   handleClick(cell){
@@ -69,7 +118,7 @@ export default class App extends React.Component {
       if(this.state.user.clicks>=this.state.grainCost){
         console.log("grain would be placed")
         //place grain
-
+        this.setState({data:(this.insertGrain(this.state.data, cell.x,cell.y,{color:(this.state.user.color)}))})
         //and lower user clicks
         this.updateUser(this.state.user.x, this.state.user.y, this.state.grainCost)
       }else{console.log("no grain placed")}
