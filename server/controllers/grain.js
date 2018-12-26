8129a4dfe1b79a52169260e3e8dfbd1844da7151
j <github@ergdyne.com>
2018-12-26 18:01:30 -0500

    feat($playerInput): basic server side validation of grain/move

--- server/controllers/grain.js
+++ server/controllers/grain.js
@@ -1,36 +1,40 @@
-import {grain, grain_location, player_data} from '../models/models'
+import {grain, grain_location} from '../models/models'
 import grainFlow from '../operations/grainFlow'
+import playerData from '../operations/playerData'
+import settingsJSON from '../operations/settingsJSON'
+
 //TODO do something like transactions or include relationships fully in models
 module.exports={
   place(req,res){
     const pkPlayer = parseInt(req.session.passport.user)||0
-    return player_data
-    .findOne({where:{pk_player:pkPlayer}})
-    .then(p => {
-      const player = p.toJSON()
-      const cellX=parseInt(req.body.x)
-      const cellY=parseInt(req.body.y)
-      if(player.x!==cellX || player.y!==cellY){
-        res.status(409).send(p)
-      }else{
-        return grain
-        .create({
-            signal: ((req.body.hasOwnProperty('signal'))?req.body.signal:''),
-            fk_player: pkPlayer
-        })
-        .then(g=>{
-          return grain_location
+    playerData(pkPlayer)
+    .then(player => {
+      settingsJSON()
+      .then(settings =>{
+        const cellX=parseInt(req.body.x)
+        const cellY=parseInt(req.body.y)
+        if(player.x!==cellX || player.y!==cellY || player.clicks < settings.GRAIN_COST){
+          res.status(409).send(p)
+        }else{
+          return grain
           .create({
-              fk_grain: g.toJSON().pk_grain,
-              x: cellX,
-              y: cellY
+              signal: ((req.body.hasOwnProperty('signal'))?req.body.signal:''),
+              fk_player: pkPlayer
           })
-          .then(r=>{
-            res.status(201).send(r)
-            grainFlow(1)
+          .then(g=>{
+            return grain_location
+            .create({
+                fk_grain: g.toJSON().pk_grain,
+                x: cellX,
+                y: cellY
+            })
+            .then(r=>{
+              grainFlow(1)
+              res.status(201).send(r)
+            })
           })
-        })
-      }
+        }
+      })
     })
     .catch(e=>res.status(400).send(e))
   }
